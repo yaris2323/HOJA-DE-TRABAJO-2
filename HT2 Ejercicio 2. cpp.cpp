2bba9bcb9a2c11c256a016f2b228cedd786f90c8
clarita <ymolinag@miumg.edu.gt>
2020-05-01 23:13:32 -0600

    Primer Repositorio

---
+++ HT2 Ejercicio 2. cpp.cpp
@@ -0,0 +1,56 @@
+/*La asociación solidarista JKL de la empresa el aceite feliz, le solicita su ayuda para que escriba una aplicación que ayude
+ a evaluar si se le puede otorgar o no, un préstamo a una persona que lo solicitante. 
+ La evaluación se realizará con los siguientes criterios:
+ 
+ 
+ Cada criterio otorga un puntaje.  Para que a la persona se le pueda otorgar el préstamo, debe de alcanzar un puntaje total 
+ superior de 20 pts. El programa debe solicitar código de empleado, nombre, salario, el tipo de vivienda donde vive, 
+ si posee tarjeta de crédito y si tiene carga familiar.  
+  El programa deberá de desplegar en pantalla si el préstamo fue 
+ otorgado al solicitante y a su vez esta información debe grabarse en un archivo.  También se le requiere que se generé 
+ un reporte de cuantos prestamos fueron otorgados y cuantos no según lo almacenado en el archivo
+ */
+ 
+ #include<iostream>
+#include<string>
+#include<iostream>
+
+using namespace std;
+int main(){
+	string vivienda;
+   	string tarcredi;
+    string sueldo;
+	string familia;
+	string prestamo,cliente;
+	 
+    
+// estructructura
+
+      cout<<"tipo de vivienda: ";cin>>vivienda;cout<<endl; 
+      
+      cout<<"obtiene targeta de credito:  ";cin>>tarcredi;cout<<endl; 
+      cout<<"sueldo que gana: ";cin>>sueldo;cout<<endl;  
+      cout<<"familia acargo: ";cin>>familia;cout<<endl;  
+      cout<<"posee prestamos, : ";cin>>prestamo;cout<<endl;  
+    
+      
+      
+      
+	  
+	  
+	  
+	  	 
+    cliente=prestamo
+      if (cliente>si){  
+           cout<<endl;  
+           cout<<"ortoga el prestamo : "<<cliente;  
+      }  
+      else  
+      {  
+           cout<<endl;  
+           cout<<"no otorga el prestamo : "<<prom;  
+      }  
+      return 0;  
+ }  
+    
+    
